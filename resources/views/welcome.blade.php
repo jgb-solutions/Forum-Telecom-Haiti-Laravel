ad4c2ac6879eec38edde4901e2fffb34e95de623
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-04-20 18:46:16 -0500

    updated the welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Forum Telecom Haiti</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Forum Telecom Haiti</div>
             </div>
         </div>
     </body>
