575db67d549b8e5420c187ff1afddb1331f65f41
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-04-27 13:01:55 -0500

    Quick Commit

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -208,7 +208,7 @@
                                 </h4>
                             </div>
                             <div class="modal-body">
-                                <p class="text-left">﻿<small>Jean Gérard Bousiquot, originaire de Port-au-Prince, Haïti, est designer, dévloppeur web/mobile et graphiste. Tout jeune il a entrepris des projets interessants parmi lesquels <a target="blank" href="http://tikwenpam.net">Ti&nbsp;Kwen&nbsp;Pam</a>, un site de partage de musiques et vidéos très populaire dans le milieu haitien.</small></p>
+                                <p class="text-left">﻿Biographie de Frederic Jean Kedson.</p>
                             </div>
                             <div class="modal-footer">
                                <a
@@ -253,7 +253,11 @@
                                 </h4>
                             </div>
                             <div class="modal-body">
-                                <p class="text-left">﻿<small>Jean Gérard Bousiquot, originaire de Port-au-Prince, Haïti, est designer, dévloppeur web/mobile et graphiste. Tout jeune il a entrepris des projets interessants parmi lesquels <a target="blank" href="http://tikwenpam.net">Ti&nbsp;Kwen&nbsp;Pam</a>, un site de partage de musiques et vidéos très populaire dans le milieu haitien.</small></p>
+                                <p class="text-left">﻿Jean Gérard Bousiquot, originaire de Port-au-Prince, Haïti, est designer, dévloppeur web/mobile et graphiste. Tout jeune il a entrepris des projets interessants parmi lesquels <a target="blank" href="http://tikwenpam.net">Ti&nbsp;Kwen&nbsp;Pam</a>, un site de partage de musiques et vidéos très populaire dans le milieu haitien.</p>
+
+                                <p class="text-left">
+                                    Il est passionément curieux et cherche toujours à augmenter ses connaissances en lisant de bons livres, mais qui traitent des sujets rodant autour de l'informatique. Il cherche presqu'à devenir le Mark Zuckerberg du pays en passant la majorité de son temps devant un ordinateur à dévlopper des produits technologiques pour le web et les plateformes mobiles tels que Android et iOS.
+                                </p>
                             </div>
                             <div class="modal-footer">
                                <a
@@ -266,6 +270,12 @@
                                     target="blank"
                                     class="btn btn-primary" role="button">
                                     <i class="fa fa-linkedin"></i> LinkedIn
+                                </a>
+                                <a
+                                    href="http://jgbnd.com"
+                                    target="blank"
+                                    class="btn btn-default" role="button">
+                                    <i class="fa fa-globe"></i> Site Web
                                 </a>
                             </div>
                         </div>
