9466ceb40d25a7236482d8a11fe9bb598e658154
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-04-28 15:04:34 -0500

    Quick Commit

--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -5,13 +5,13 @@
             <div class="col-sm-12">
                 <h2>Sponsor Niveau 1</h2>
                 <div class="row">
-                    <div class="col-sm-6 col-xs-6 text-center">
+                    <div class="col-sm-3 col-xs-6 text-center">
                         <img src="img/conatel-logo-500x500.png"
                             class="img-responsive img-rounded sponsors"
                             alt="CONATEL"
                             title="CONATEL">
                     </div>
-                    <div class="col-sm-6 col-xs-6 text-center">
+                    <div class="col-sm-3 col-xs-6 text-center">
                         <img src="img/logo-natcom.jpg"
                             class="img-responsive img-rounded sponsors"
                             alt="Natcom"
@@ -25,34 +25,34 @@
                 <h2>Sponsor Niveau 2</h2>
 
                 <div class="row">
-                    <div class="col-sm-3 col-xs-6 text-center">
+                    <div class="col-sm-2 col-xs-6 text-center">
                         <img src="img/juno7.jpg"
                             class="img-responsive img-rounded sponsors"
                             alt="Juno7"
                             title="Juno7">
                     </div>
-                    <div class="col-sm-3 col-xs-6 text-center">
+                    <div class="col-sm-2 col-xs-6 text-center">
                         <img src="img/mds.jpg"
                             class="img-responsive img-rounded sponsors"
                             alt="MDS"
                             title="MDS">
                     </div>
 
-                    <div class="col-sm-3 col-xs-6 text-center">
+                    <div class="col-sm-2 col-xs-6 text-center">
                         <img src="img/ti-kwen-pam.jpg"
                             class="img-responsive img-rounded sponsors"
                             alt="Ti Kwen Pam http://tikwenpam.net"
                             title="Ti Kwen Pam - Rezo Sosyal Mizik Ayisyen">
                     </div>
-                    <div class="col-sm-3 col-xs-6 text-center">
+                    <div class="col-sm-2 col-xs-6 text-center">
                         <img src="img/eliacin-500x500.jpg"
                             class="img-responsive img-rounded sponsors">
                     </div>
-                    <div class="col-sm-3 col-xs-6 text-center">
+                    <div class="col-sm-2 col-xs-6 text-center">
                         <img src="img/new-vision-500x500.jpg"
                             class="img-responsive img-rounded sponsors">
                     </div>
-                    <div class="col-sm-3 col-xs-6 text-center">
+                    <div class="col-sm-2 col-xs-6 text-center">
                         <img src="img/iret.jpg"
                             class="img-responsive img-rounded sponsors">
                     </div>
