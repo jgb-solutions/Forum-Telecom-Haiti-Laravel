ebf7ee1f07aa9b4f1c213d8c26296ccfa9e87abe
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-04-30 18:34:54 -0500

    Quick Commit

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -9,9 +9,7 @@
 
     @include('beautymail::templates.ark.contentStart')
         <p>
-            Hello {{ $firstname . ' ' . $lastname }}! Nous vous remercions pour l'intérêt que vous portez a ce forum. Votre participation a été confirmée. Prière de présenter une pièce d'indentité le jour du forum. <br>
-
-            Heure d'ouverture 9:00 AM.
+            Hello {{ $firstname . ' ' . $lastname }}! Nous vous remercions pour l'intérêt que vous portez a ce forum. Votre participation a été confirmée. Prière de présenter une pièce d'indentité le jour du forum. Heure d'ouverture 9:00 AM.
         </p>
 
         <p>
