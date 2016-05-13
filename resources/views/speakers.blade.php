14e5ca8ae586560c71187acd8891f7bae251ea2d
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-05-13 18:15:04 -0500

    Quick Commit

--- resources/views/speakers.blade.php
+++ resources/views/speakers.blade.php
@@ -51,6 +51,100 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <a data-toggle="modal" href='#amos-modal'>
+                            <img
+                                src="img/amos-square.jpg"
+                                alt="Amos SINCIR"
+                                class="img-responsive img-rounded team">
+                            <div class="caption">
+                                <h4>Amos SINCIR<br>
+                                    <small>Jeune Président d'Haiti</small>
+                                </h4>
+                            </div>
+                        </a>
+
+                        <div class="modal fade" id="amos-modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" hidden="true">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title text-center">
+                                            Amos SINCIR <br>
+                                            <small>Jeune Président d'Haiti</small>
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img
+                                            src="img/amos.png"
+                                            alt="stephane Vincent"
+                                            class="img-responsive">
+                                    </div>
+                                    <div class="modal-footer"  hidden="true">
+                                        <a
+                                            href="https://twitter.com/Vincentbel"
+                                            class="btn btn-info" role="button"
+                                            target="blank">
+                                            <i class="fa fa-twitter"></i> Twitter</a>
+                                        <a
+                                            href="https://www.linkedin.com/in/stephane-Vincent-b4473834"
+                                            target="blank"
+                                            class="btn btn-primary" role="button">
+                                            <i class="fa fa-linkedin"></i> LinkedIn
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <a data-toggle="modal" href='#james-modal'>
+                            <img
+                                src="img/james-square.jpg"
+                                alt="James-Taylor LOUIS"
+                                class="img-responsive img-rounded team">
+                            <div class="caption">
+                                <h4>James-Taylor LOUIS<br>
+                                    <small>Président Directeur Général <a href="http://10kont.com" target="blank">10kont.com</a></small>
+                                </h4>
+                            </div>
+                        </a>
+
+                        <div class="modal fade" id="james-modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" hidden="true">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title text-center">
+                                            James-Taylor LOUIS <br>
+                                            <small>Président Directeur Général <a href="http://10kont.com" target="blank">10kont.com</a></small>
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img
+                                            src="img/james.png"
+                                            alt="James-Taylor LOUIS"
+                                            class="img-responsive">
+                                    </div>
+                                    <div class="modal-footer"  hidden="true">
+                                        <a
+                                            href="https://twitter.com/Vincentbel"
+                                            class="btn btn-info" role="button"
+                                            target="blank">
+                                            <i class="fa fa-twitter"></i> Twitter</a>
+                                        <a
+                                            href="https://www.linkedin.com/in/stephane-Vincent-b4473834"
+                                            target="blank"
+                                            class="btn btn-primary" role="button">
+                                            <i class="fa fa-linkedin"></i> LinkedIn
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
