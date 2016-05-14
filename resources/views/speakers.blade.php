ee178d8ff9ae608182249b9579f527035bcb0c55
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-05-14 14:52:06 -0500

    Quick Commit

--- resources/views/speakers.blade.php
+++ resources/views/speakers.blade.php
@@ -145,6 +145,266 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <a data-toggle="modal" href='#athanase-modal'>
+                            <img
+                                src="img/athanase-square.jpg"
+                                alt="Athanase Louis"
+                                class="img-responsive img-rounded team">
+                            <div class="caption">
+                                <h4>Athanase Louis<br>
+                                    <small>Président Directeur Général <a href="http://10kont.com" target="blank">10kont.com</a></small>
+                                </h4>
+                            </div>
+                        </a>
+
+                        <div class="modal fade" id="athanase-modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" hidden="true">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title text-center">
+                                           Athanase Louis <br>
+                                            <small>PDG Atalou Microsystem</small>
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img
+                                            src="img/athanase.png"
+                                            alt="Athanase Louis"
+                                            class="img-responsive">
+                                    </div>
+                                    <div class="modal-footer"  hidden="true">
+                                        <a
+                                            href="https://twitter.com/Vincentbel"
+                                            class="btn btn-info" role="button"
+                                            target="blank">
+                                            <i class="fa fa-twitter"></i> Twitter</a>
+                                        <a
+                                            href="https://www.linkedin.com/in/stephane-Vincent-b4473834"
+                                            target="blank"
+                                            class="btn btn-primary" role="button">
+                                            <i class="fa fa-linkedin"></i> LinkedIn
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <a data-toggle="modal" href='#guy-modal'>
+                            <img
+                                src="img/guy-square.jpg"
+                                alt="Joseph Guyler C. Delva"
+                                class="img-responsive img-rounded team">
+                            <div class="caption">
+                                <h4>Joseph Guyler C. Delva<br>
+                                    <small>PDG de Haitian Caribbean News Network</small>
+                                </h4>
+                            </div>
+                        </a>
+
+                        <div class="modal fade" id="guy-modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" hidden="true">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title text-center">
+                                            Joseph Guyler C. Delva <br>
+                                            <small>PDG de Haitian Caribbean News Network</small>
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img
+                                            src="img/guy.png"
+                                            alt="James-Taylor LOUIS"
+                                            class="img-responsive">
+                                    </div>
+                                    <div class="modal-footer"  hidden="true">
+                                        <a
+                                            href="https://twitter.com/Vincentbel"
+                                            class="btn btn-info" role="button"
+                                            target="blank">
+                                            <i class="fa fa-twitter"></i> Twitter</a>
+                                        <a
+                                            href="https://www.linkedin.com/in/stephane-Vincent-b4473834"
+                                            target="blank"
+                                            class="btn btn-primary" role="button">
+                                            <i class="fa fa-linkedin"></i> LinkedIn
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <a data-toggle="modal" href='#frederic-modal'>
+                            <img
+                                src="img/frederic-square.jpg"
+                                alt="Frédérique Pierre"
+                                class="img-responsive img-rounded team">
+                            <div class="caption">
+                                <h4>Frédérique Pierre<br>
+                                    <small>PDG de 1 Click</small>
+                                </h4>
+                            </div>
+                        </a>
+
+                        <div class="modal fade" id="frederic-modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" hidden="true">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title text-center">
+                                            Frédérique Pierre <br>
+                                            <small>PDG de 1 Click</small>
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img
+                                            src="img/frederic.png"
+                                            alt="Frédérique Pierre"
+                                            class="img-responsive">
+                                    </div>
+                                    <div class="modal-footer"  hidden="true">
+                                        <a
+                                            href="https://twitter.com/Vincentbel"
+                                            class="btn btn-info" role="button"
+                                            target="blank">
+                                            <i class="fa fa-twitter"></i> Twitter</a>
+                                        <a
+                                            href="https://www.linkedin.com/in/stephane-Vincent-b4473834"
+                                            target="blank"
+                                            class="btn btn-primary" role="button">
+                                            <i class="fa fa-linkedin"></i> LinkedIn
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <a data-toggle="modal" href='#giovanni-modal'>
+                            <img
+                                src="img/giovanni-square.jpg"
+                                alt="Giovanni Michele TOGLIA"
+                                class="img-responsive img-rounded team">
+                            <div class="caption">
+                                <h4>Giovanni Michele TOGLIA<br>
+                                    <small>Directeur Technique Presso THESS ONLINE</small>
+                                </h4>
+                            </div>
+                        </a>
+
+                        <div class="modal fade" id="giovanni-modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" hidden="true">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title text-center">
+                                            Giovanni Michele TOGLIA <br>
+                                            <small>Directeur Technique Presso THESS ONLINE</small>
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img
+                                            src="img/giovanni.png"
+                                            alt="James-Taylor LOUIS"
+                                            class="img-responsive">
+                                    </div>
+                                    <div class="modal-footer"  hidden="true">
+                                        <a
+                                            href="https://twitter.com/Vincentbel"
+                                            class="btn btn-info" role="button"
+                                            target="blank">
+                                            <i class="fa fa-twitter"></i> Twitter</a>
+                                        <a
+                                            href="https://www.linkedin.com/in/stephane-Vincent-b4473834"
+                                            target="blank"
+                                            class="btn btn-primary" role="button">
+                                            <i class="fa fa-linkedin"></i> LinkedIn
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <a data-toggle="modal" href='#sam-modal'>
+                            <img
+                                src="img/sam-square.jpg"
+                                alt="Samuel Pierre"
+                                class="img-responsive img-rounded team">
+                            <div class="caption">
+                                <h4>Samuel Pierre<br>
+                                    <small>PDG de TCS</small>
+                                </h4>
+                            </div>
+                        </a>
+
+                        <div class="modal fade" id="sam-modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" hidden="true">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title text-center">
+                                            Samuel Pierre <br>
+                                            <small>PDG de TCS</small>
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img
+                                            src="img/sam.png"
+                                            alt="Samuel Pierre"
+                                            class="img-responsive">
+                                    </div>
+                                    <div class="modal-footer"  hidden="true">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 col-xs-6">
+                        <a data-toggle="modal" href='#mic-modal'>
+                            <img
+                                src="img/mic-square.jpg"
+                                alt="Michael VERCH"
+                                class="img-responsive img-rounded team">
+                            <div class="caption">
+                                <h4>Michael VERCH<br>
+                                    <small>Vice-président AHTIC</small>
+                                </h4>
+                            </div>
+                        </a>
+
+                        <div class="modal fade" id="mic-modal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" hidden="true">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title text-center">
+                                            Michael VERCH <br>
+                                            <small>Vice-président AHTIC</small>
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img
+                                            src="img/mic.png"
+                                            alt="Michael VERCH"
+                                            class="img-responsive">
+                                    </div>
+                                    <div class="modal-footer"  hidden="true">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
