ff40c43d5937f25a262df14b32f9022119f8655a
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-05-16 19:59:36 -0500

    Quick Commit

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -13,41 +13,32 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <div>
-                    <h2 class="text-center"> Participants inscrits à l'événement</h2>
+                <h2 class="text-center"> Participants inscrits à l'événement</h2>
 
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Prénom</th>
-                                    <th>Nom</th>
-                                    <th>Secteur Activité</th>
-                                    <th>Ecole Professionnelle ou Université</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($users as $user)
-                                <tr>
-                                    <td>{{ $user->firstname }}</td>
-                                    <td>{{ $user->lastname }}</td>
-                                    <td>{{ $user->activite }}</td>
-                                    <td>{{ $user->school }}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Prénom</th>
+                                <th>Nom</th>
+                                <th>Secteur Activité</th>
+                                <th>Ecole Professionnelle ou Université</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->firstname }}</td>
+                                <td>{{ $user->lastname }}</td>
+                                <td>{{ $user->activite }}</td>
+                                <td>{{ $user->school }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
 
-                        <div class="text-center">
-                            {{ $users->links() }}
-                        </div>
-                </div>
-
+                    <div class="text-center">
+                        {{ $users->links() }}
+                    </div>
             </div>
         </div>
     </div>
